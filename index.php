7abe0df42b12c7393c9da2418e2747e059cbf183
Mathias Bynens <mathias@qiwi.be>
2011-06-21 09:53:31 +0200

    Use shorter but more readable variable names.

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 require 'config.php';
 
-$redirectDestination = DEFAULT_URL . '/';
+$url = DEFAULT_URL . '/';
 
 if (isset($_GET['slug'])) {
 
 	$slug = preg_replace('/[^a-z0-9]/si', '', $_GET['slug']);
 	if (is_numeric($slug) && strlen($slug) > 8) {
-		$redirectDestination = 'http://twitter.com/' . TWITTER_USERNAME . '/status/' . $slug;
+		$url = 'http://twitter.com/' . TWITTER_USERNAME . '/status/' . $slug;
 
 	} else {
 
@@ -18,11 +18,11 @@ if (isset($_GET['slug'])) {
 		$escapedSlug = $db->real_escape_string($slug);
 		$redirectResult = $db->query('SELECT url FROM redirect WHERE slug = "' . $escapedSlug . '"');
 
-		if ($redirectResult !== false && $redirectResult->num_rows > 0) {
+		if ($redirectResult && $redirectResult->num_rows > 0) {
 			$db->query('UPDATE redirect SET hits = hits + 1 WHERE slug = "' . $escapedSlug . '"');
-			$redirectDestination = $redirectResult->fetch_object()->url;
+			$url = $redirectResult->fetch_object()->url;
 		} else {
-			$redirectDestination = DEFAULT_URL . $_SERVER['REQUEST_URI'];
+			$url = DEFAULT_URL . $_SERVER['REQUEST_URI'];
 		}
 
 		$db->close();
@@ -30,16 +30,16 @@ if (isset($_GET['slug'])) {
 	}
 }
 
-header('Location: ' . $redirectDestination, null, 301);
+header('Location: ' . $url, null, 301);
 
 ?>
 <!DOCTYPE html>
 <meta charset=utf-8>
 <title>Redirecting…</title>
-<meta http-equiv=refresh content="0; URL=<?php echo $redirectDestination; ?>">
-<a href="<?php echo $redirectDestination; ?>">Click here to continue to <?php echo $redirectDestination; ?>…</a>
+<meta http-equiv=refresh content="0; URL=<?php echo $url; ?>">
+<a href="<?php echo $url; ?>">Click here to continue to <?php echo $url; ?>…</a>
 <script>
 	try {
-		document.location.href = '<?php echo $redirectDestination; ?>';
+		document.location.href = '<?php echo $url; ?>';
 	} catch(e) {}
 </script>
