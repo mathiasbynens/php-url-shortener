f8d4578e974b8c2ae288e43643796254477c6814
Mathias Bynens <mathias@qiwi.be>
2011-11-03 09:15:03 +0100

    Update example `GOOGLE_PLUS_ID` to match my new G+ account.

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('MYSQL_USER', 'user');
 define('MYSQL_PASSWORD', 'password');
 define('MYSQL_DATABASE', 'database');
 define('TWITTER_USERNAME', 'mathias');
-define('GOOGLE_PLUS_ID', '116553353277057965424');
+define('GOOGLE_PLUS_ID', '106697091536876736486');
 define('SHORT_URL', 'http://mths.be/'); // include the trailing slash!
 define('DEFAULT_URL', 'http://mathiasbynens.be'); // omit the trailing slash!
 
