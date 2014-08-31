bd49941fc41a2c6a99d973b86598c19b14f6e2fc
Mathias Bynens <mathias@qiwi.be>
2014-08-31 19:32:56 +0200

    Update placeholder values to encourage HTTPS usage

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('MYSQL_PASSWORD', 'password');
 define('MYSQL_DATABASE', 'database');
 define('TWITTER_USERNAME', 'mathias');
 define('GOOGLE_PLUS_ID', '106697091536876736486');
-define('SHORT_URL', 'http://mths.be/'); // include the trailing slash!
-define('DEFAULT_URL', 'http://mathiasbynens.be'); // omit the trailing slash!
+define('SHORT_URL', 'https://mths.be/'); // include the trailing slash!
+define('DEFAULT_URL', 'https://mathiasbynens.be'); // omit the trailing slash!
 
 ?>
