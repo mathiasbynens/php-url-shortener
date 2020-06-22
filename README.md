cc1983827481e8566b9e4ce202431a1964132b44
Medard Mandane <medardm13@gmail.com>
2020-06-22 23:47:28 +0800

    Include MYSQL_PORT when connecting to database and require config-example.php if config.php doesn't exist
    - update README

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Requires PHP ≥ 5.4.0 or higher.
 
 1. Download the source code as located within this repository, and upload it to your web server.
 2. Use `database.sql` to create the `redirect` table in a database of choice. (Do *not* delete the `INSERT` statement on [line 10](https://github.com/mathiasbynens/php-url-shortener/blob/f64ee342246fa5bf0340641372680a2d398afc79/database.sql#L10) as it is needed to initialize the database.)
-3. Edit `config.php` and enter your database credentials.
-4. For additional *security through obscurity™*, consider renaming `shorten.php` to a secret file name of your choosing and tweaking the `.htaccess` file ([line 3](https://github.com/mathiasbynens/php-url-shortener/blob/f64ee342246fa5bf0340641372680a2d398afc79/.htaccess#L3)) accordingly.
+3. Rename `config-example.php` to `config.php`.
+4. Edit `config.php` and enter your database credentials.
+5. For additional *security through obscurity™*, consider renaming `shorten.php` to a secret file name of your choosing and tweaking the `.htaccess` file ([line 3](https://github.com/mathiasbynens/php-url-shortener/blob/f64ee342246fa5bf0340641372680a2d398afc79/.htaccess#L3)) accordingly.
 
 ## Features
 
@@ -49,4 +50,5 @@ This script is available under the MIT license.
 
 * [Peter Beverloo](http://peter.sh/)
 * [Tomislav Biscan](https://github.com/B-Scan)
+* [Medard Mandane](https://github.com/medardm/)
 
--- index.php
+++ index.php
@@ -1,46 +1,53 @@
 <?php
 
-require 'config.php';
+if (file_exists('config.php')) {
+    require 'config.php';
+} else {
+    require 'config-example.php';
+}
 
 $url = DEFAULT_URL . '/';
 
 if (isset($_GET['slug'])) {
+    $slug = $_GET['slug'];
 
-	$slug = $_GET['slug'];
+    if ('@' == $slug) {
+        $url = 'https://twitter.com/' . TWITTER_USERNAME;
+    } else {
+        if (' ' == $slug) {
+            // +
+            $url = 'https://plus.google.com/u/0/' . GOOGLE_PLUS_ID . '/posts';
+        } else {
+            $slug = preg_replace('/[^a-z0-9]/si', '', $slug);
 
-	if ('@' == $slug) {
-		$url = 'https://twitter.com/' . TWITTER_USERNAME;
-	} else if (' ' == $slug) { // +
-		$url = 'https://plus.google.com/u/0/' . GOOGLE_PLUS_ID . '/posts';
-	} else {
+            if (is_numeric($slug) && strlen($slug) > 8) {
+                $url = 'https://twitter.com/' . TWITTER_USERNAME . '/status/' . $slug;
+            } else {
+                $db = new MySQLi(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_PORT);
+                $db->set_charset('utf8mb4');
 
-		$slug = preg_replace('/[^a-z0-9]/si', '', $slug);
+                $escapedSlug = $db->real_escape_string($slug);
+                $redirectResult = $db->query('SELECT url FROM redirect WHERE slug = "' . $escapedSlug . '"');
 
-		if (is_numeric($slug) && strlen($slug) > 8) {
-			$url = 'https://twitter.com/' . TWITTER_USERNAME . '/status/' . $slug;
-		} else {
+                if ($redirectResult && $redirectResult->num_rows > 0) {
+                    $db->query('UPDATE redirect SET hits = hits + 1 WHERE slug = "' . $escapedSlug . '"');
+                    $url = $redirectResult->fetch_object()->url;
+                } else {
+                    $url = DEFAULT_URL . $_SERVER['REQUEST_URI'];
+                }
 
-			$db = new MySQLi(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE);
-			$db->set_charset('utf8mb4');
-
-			$escapedSlug = $db->real_escape_string($slug);
-			$redirectResult = $db->query('SELECT url FROM redirect WHERE slug = "' . $escapedSlug . '"');
-
-			if ($redirectResult && $redirectResult->num_rows > 0) {
-				$db->query('UPDATE redirect SET hits = hits + 1 WHERE slug = "' . $escapedSlug . '"');
-				$url = $redirectResult->fetch_object()->url;
-			} else {
-				$url = DEFAULT_URL . $_SERVER['REQUEST_URI'];
-			}
-
-			$db->close();
-
-		}
-	}
+                $db->close();
+            }
+        }
+    }
 }
 
 header('Location: ' . $url, null, 301);
 
 $attributeValue = htmlspecialchars($url);
 ?>
-<meta http-equiv=refresh content="0;URL=<?php echo $attributeValue; ?>"><a href="<?php echo $attributeValue; ?>">Continue</a><script>location.href=<?php echo json_encode($url, JSON_HEX_TAG | JSON_UNESCAPED_SLASHES); ?></script>
+<meta
+        http-equiv=refresh
+        content="0;URL=<?php echo $attributeValue; ?>"
+><a href="<?php echo $attributeValue; ?>">Continue</a>
+<script>location.href =<?php echo json_encode($url, JSON_HEX_TAG | JSON_UNESCAPED_SLASHES); ?></script>
