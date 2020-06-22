048e187821bdd7f0e710760a29ae718f440ac18e
Medard Mandane <medardm13@gmail.com>
2020-06-22 23:44:02 +0800

    Add config-example.php and remove from repo and gitignore config.php

--- config-example.php
+++ config-example.php
@@ -1,12 +1,11 @@
 <?php
 
 define('MYSQL_HOST', 'localhost');
-define('MYSQL_USER', 'user');
-define('MYSQL_PASSWORD', 'password');
-define('MYSQL_DATABASE', 'database');
-define('TWITTER_USERNAME', 'mathias');
+define('MYSQL_USER', 'test');
+define('MYSQL_DATABASE', 'url_redirect');
+define('MYSQL_PASSWORD', 'test');
+define('TWITTER_USERNAME', 'sparkpointio');
 define('GOOGLE_PLUS_ID', '106697091536876736486');
 define('SHORT_URL', 'https://mths.be/'); // include the trailing slash!
 define('DEFAULT_URL', 'https://mathiasbynens.be'); // omit the trailing slash!
-
 ?>
